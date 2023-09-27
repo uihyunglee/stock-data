dc58a960b2104aa433f3e440a8693e977a9909d8
uihyunglee <uihyunglee.tech@gmail.com>
2023-09-27 13:23:24 +0900

    Add check_creon_system

--- updater_price.py
+++ updater_price.py
@@ -6,3 +6,25 @@ import json
 import numpy as np
 import pandas as pd
 import pymysql
+
+# 크레온 플러스 시스템 점검
+cpStatus = win32com.client.Dispatch('CpUtil.CpCybos') # 시스템 상태 정보
+cpTradeUtil = win32com.client.Dispatch('CpTrade.CpTdUtil') # 주문 관련 도구
+
+def check_creon_system():
+    # 관리자 권한으로 프로세스 실행 여부
+    if not ctypes.windll.shell32.IsUserAnAdmin():
+        print('check_creon_system() : admin user -> FAILED')
+        return False
+
+    # 연결 여부 체크
+    if (cpStatus.IsConnect == 0):
+        print('check_creon_system() : connect to server -> FAILED')
+        return False
+
+    # 주문 관련 초기화
+    if (cpTradeUtil.TradeInit(0) != 0):
+        print('check_creon_system() : init trade -> FAILED')
+        return False
+
+    return True
