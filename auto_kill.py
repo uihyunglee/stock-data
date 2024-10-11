6d2f50881f4f29f14e068b0873105fc78dd3c609
uihyunglee <uihyunglee.tech@gmail.com>
2024-10-11 15:53:16 +0900

    FEAT: Add creon plus kill module

---
+++ auto_kill.py
@@ -0,0 +1,6 @@
+import os
+
+os.system('taskkill /IM coStarter* /F /T')
+os.system('taskkill /IM CpStart* /F /T')
+os.system('wmic process where "name like \'%coStarter%\'" call terminate')
+os.system('wmic process where "name like \'%CpStart%\'" call terminate')
